651644c60201e69e8be3e18ef409d98afb24daa0
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-21 20:08:20 +0000

    Added Selected Slot & All Items Method

--- common/src/main/java/com/rabbitminers/buildersbundle/container/SatchelInventory.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/container/SatchelInventory.java
@@ -7,17 +7,44 @@ import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class SatchelInventory extends SimpleContainer {
     private final ItemStack stack;
+    private int selectedSlot;
     public SatchelInventory(ItemStack stack, int count) {
         super(count);
         this.stack = stack;
+        this.selectedSlot = 0;
         readItemStack();
+    }
+
+    public int getSelectedSlot() {
+        return this.selectedSlot;
+    }
+
+    public ItemStack getSelectedItem() {
+        return this.getItem(this.selectedSlot);
+    }
+
+    public void setSelectedSlot(int slot) {
+        this.selectedSlot = slot;
+    }
+
+    public List<ItemStack> getAllItems() {
+        return IntStream.range(0, getContainerSize())
+                .mapToObj(this::getItem)
+                .filter(stack -> !stack.isEmpty())
+                .collect(Collectors.toList());
     }
 
     public void readItemStack() {
         if (stack.getTag() != null) {
             readNBT(stack.getTag());
+            readSelectedItem(stack.getTag());
         }
     }
 
@@ -28,6 +55,19 @@ public class SatchelInventory extends SimpleContainer {
             writeNBT(stack.getOrCreateTag());
         }
     }
+
+    private void readSelectedItem(CompoundTag tag) {
+        if (tag.contains("SelectedSlot")) {
+            this.selectedSlot = tag.getInt("SelectedSlot");
+        } else {
+            this.selectedSlot = 0;
+        }
+    }
+
+    private void writeSelectedItem(CompoundTag tag) {
+        tag.putInt("SelectedSlot", this.selectedSlot);
+    }
+
     private void readNBT(CompoundTag compound) {
         final NonNullList<ItemStack> list = NonNullList.withSize(getContainerSize(), ItemStack.EMPTY);
         ContainerHelper.loadAllItems(compound, list);

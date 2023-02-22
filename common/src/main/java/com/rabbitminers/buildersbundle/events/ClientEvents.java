bc52d1f191b81e8fe46857f4dab86ed3917a209b
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-22 22:21:39 +0000

    Added client post events

--- common/src/main/java/com/rabbitminers/buildersbundle/events/ClientEvents.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/events/ClientEvents.java
@@ -1,8 +1,10 @@
 package com.rabbitminers.buildersbundle.events;
 
+import com.rabbitminers.buildersbundle.registry.AllKeymaps;
 import com.rabbitminers.buildersbundle.satchel.SatchelItem;
 import dev.architectury.event.events.client.ClientRawInputEvent;
 import dev.architectury.event.events.client.ClientScreenInputEvent;
+import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 
@@ -14,5 +16,13 @@ public class ClientEvents {
 
     public static void clientEvents() {
         ClientRawInputEvent.MOUSE_SCROLLED.register(SatchelItem::cycleActiveBlock);
+        ClientTickEvent.CLIENT_POST.register(minecraft -> {
+            while (AllKeymaps.CYCLE_BACKWARDS.consumeClick()
+                    || AllKeymaps.CYCLE_FORWARDS.consumeClick()) {
+                SatchelItem.cycleSelectedBlock(minecraft,
+                        AllKeymaps.CYCLE_FORWARDS.consumeClick());
+            }
+        });
+
     }
 }

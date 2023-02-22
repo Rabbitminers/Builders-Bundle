4cbb90d29acecfce978b7fed7b4f3e35421233d7
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-22 22:22:29 +0000

    Added extra inventory utils

--- common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
@@ -1,9 +1,12 @@
 package com.rabbitminers.buildersbundle.util;
 
 import com.rabbitminers.buildersbundle.ArchitectsSatchel;
+import com.rabbitminers.buildersbundle.container.SatchelInventory;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 public class InventoryUtil {
@@ -13,5 +16,29 @@ public class InventoryUtil {
                 ? InteractionHand.OFF_HAND
                 : player.getMainHandItem().getItem() == item
                 ? InteractionHand.MAIN_HAND : null;
+    }
+
+    public static int getFirstInventoryIndex(Player player, Item item) {
+        return getFirstInventoryIndex(player.getInventory(), item);
+    }
+
+    public static int getFirstInventoryIndex(SatchelInventory inventory, Item item) {
+        for(int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack currentStack = inventory.getItem(i);
+            if (!currentStack.isEmpty() && currentStack.sameItem(new ItemStack(item))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int getFirstInventoryIndex(Inventory inventory, Item item) {
+        for(int i = 0; i < inventory.getContainerSize(); i++) {
+            ItemStack currentStack = inventory.getItem(i);
+            if (!currentStack.isEmpty() && currentStack.sameItem(new ItemStack(item))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }

8ab667121759f1716f4525979689d99d7c58f697
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-22 22:21:27 +0000

    Registered keymaps

--- common/src/main/java/com/rabbitminers/buildersbundle/ArchitectsSatchelClient.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/ArchitectsSatchelClient.java
@@ -1,15 +1,20 @@
 package com.rabbitminers.buildersbundle;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.rabbitminers.buildersbundle.events.ClientEvents;
 import com.rabbitminers.buildersbundle.events.ServerEvents;
 import com.rabbitminers.buildersbundle.gui.SatchelGui;
+import com.rabbitminers.buildersbundle.registry.AllKeymaps;
 import com.rabbitminers.buildersbundle.registry.AllMenus;
 import dev.architectury.registry.menu.MenuRegistry;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.world.inventory.InventoryMenu;
 
 public class ArchitectsSatchelClient {
     public static void clientInit() {
         MenuRegistry.registerScreenFactory(AllMenus.SATCHEL_MENU.get(), SatchelGui::new);
+
+        AllKeymaps.init();
         ClientEvents.init();
         ServerEvents.init();
     }

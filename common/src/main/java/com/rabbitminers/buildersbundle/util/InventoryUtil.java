a052dc034b402e7df883b74771e097c0a2d1dc00
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-22 22:21:03 +0000

    Added get item from hand utility

--- common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
@@ -1,4 +1,17 @@
 package com.rabbitminers.buildersbundle.util;
 
+import com.rabbitminers.buildersbundle.ArchitectsSatchel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
+
 public class InventoryUtil {
+
+    public static @Nullable InteractionHand getHandOfBundle(Player player, Item item) {
+        return player.getOffhandItem().getItem() == item
+                ? InteractionHand.OFF_HAND
+                : player.getMainHandItem().getItem() == item
+                ? InteractionHand.MAIN_HAND : null;
+    }
 }

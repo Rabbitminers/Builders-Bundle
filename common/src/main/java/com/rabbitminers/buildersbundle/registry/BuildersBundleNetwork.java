6107636df9564de350157d58f5cf1c880448bafe
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-24 20:45:37 +0000

    Added Networking Handler

---
+++ common/src/main/java/com/rabbitminers/buildersbundle/registry/BuildersBundleNetwork.java
@@ -0,0 +1,14 @@
+package com.rabbitminers.buildersbundle.registry;
+
+import com.rabbitminers.buildersbundle.ArchitectsSatchel;
+import com.rabbitminers.buildersbundle.networking.GrowItemStackPacket;
+import dev.architectury.networking.NetworkChannel;
+import net.minecraft.resources.ResourceLocation;
+
+public class BuildersBundleNetwork {
+    public static final NetworkChannel HANDLER =
+            NetworkChannel.create(new ResourceLocation(ArchitectsSatchel.MOD_ID, "main_channel"));
+    public static void init() {
+        HANDLER.register(GrowItemStackPacket.class, GrowItemStackPacket::write, GrowItemStackPacket::new, GrowItemStackPacket::handle);
+    }
+}

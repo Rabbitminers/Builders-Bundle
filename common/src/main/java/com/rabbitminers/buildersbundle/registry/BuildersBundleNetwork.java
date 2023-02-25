e882002169b531f7ac04a17e6c3c9a92dcab4103
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-25 11:49:26 +0000

    Registered SaveCompoundTagPacket

--- common/src/main/java/com/rabbitminers/buildersbundle/registry/BuildersBundleNetwork.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/registry/BuildersBundleNetwork.java
@@ -2,6 +2,8 @@ package com.rabbitminers.buildersbundle.registry;
 
 import com.rabbitminers.buildersbundle.ArchitectsSatchel;
 import com.rabbitminers.buildersbundle.networking.GrowItemStackPacket;
+import com.rabbitminers.buildersbundle.networking.InventoryUpdatePacket;
+import com.rabbitminers.buildersbundle.networking.SaveCompoundTagPacket;
 import dev.architectury.networking.NetworkChannel;
 import net.minecraft.resources.ResourceLocation;
 
@@ -9,6 +11,13 @@ public class BuildersBundleNetwork {
     public static final NetworkChannel HANDLER =
             NetworkChannel.create(new ResourceLocation(ArchitectsSatchel.MOD_ID, "main_channel"));
     public static void init() {
-        HANDLER.register(GrowItemStackPacket.class, GrowItemStackPacket::write, GrowItemStackPacket::new, GrowItemStackPacket::handle);
+        HANDLER.register(GrowItemStackPacket.class,
+                         GrowItemStackPacket::write,
+                         GrowItemStackPacket::new,
+                         GrowItemStackPacket::handle);
+        HANDLER.register(SaveCompoundTagPacket.class,
+                         SaveCompoundTagPacket::write,
+                         SaveCompoundTagPacket::new,
+                         SaveCompoundTagPacket::handle);
     }
 }

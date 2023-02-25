5f28a491649b9bbbc85dce8a81c75df55736bf08
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-25 15:56:54 +0000

    Added Find Item In Bundles Util

--- common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/util/InventoryUtil.java
@@ -2,12 +2,17 @@ package com.rabbitminers.buildersbundle.util;
 
 import com.rabbitminers.buildersbundle.ArchitectsSatchel;
 import com.rabbitminers.buildersbundle.container.SatchelInventory;
+import com.rabbitminers.buildersbundle.satchel.SatchelItem;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InventoryUtil {
 
@@ -40,5 +45,21 @@ public class InventoryUtil {
             }
         }
         return -1;
+    }
+
+    public static ItemStack findItemInBundles(Inventory inventory, Item query) {
+        List<ItemStack> bundles = inventory.items.stream()
+                .filter(item -> item.is(ArchitectsSatchel.EXAMPLE_ITEM.get()))
+                .toList();
+
+        for (ItemStack bundle : bundles) {
+            SatchelInventory bundleInventory = SatchelItem.getInventory(bundle);
+            int index = InventoryUtil
+                    .getFirstInventoryIndex(bundleInventory, query);
+            if (index != -1)
+                return bundleInventory.getItem(index);
+        }
+
+        return ItemStack.EMPTY;
     }
 }

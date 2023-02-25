4f9d208a3899572573d23a330ec928a2e3bb893e
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-25 11:49:07 +0000

    Registered Client Events

--- common/src/main/java/com/rabbitminers/buildersbundle/ArchitectsSatchelClient.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/ArchitectsSatchelClient.java
@@ -1,14 +1,10 @@
 package com.rabbitminers.buildersbundle;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import com.rabbitminers.buildersbundle.events.ClientEvents;
-import com.rabbitminers.buildersbundle.events.ServerEvents;
-import com.rabbitminers.buildersbundle.gui.SatchelGui;
+import com.rabbitminers.buildersbundle.container.SatchelGui;
 import com.rabbitminers.buildersbundle.registry.AllKeymaps;
 import com.rabbitminers.buildersbundle.registry.AllMenus;
 import dev.architectury.registry.menu.MenuRegistry;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.world.inventory.InventoryMenu;
 
 public class ArchitectsSatchelClient {
     public static void clientInit() {

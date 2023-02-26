ce1f392aa7de1b3e26e6fb7da605fe985e1e307a
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-26 20:32:04 +0000

    Added Cycling To Placement Mode On Key Press

--- common/src/main/java/com/rabbitminers/buildersbundle/events/ClientEvents.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/events/ClientEvents.java
@@ -1,11 +1,18 @@
 package com.rabbitminers.buildersbundle.events;
 
+import com.rabbitminers.buildersbundle.container.SatchelInventory;
+import com.rabbitminers.buildersbundle.registry.BuildersBundleItems;
 import com.rabbitminers.buildersbundle.registry.BuildersBundleKeymaps;
 import com.rabbitminers.buildersbundle.satchel.SatchelItem;
+import com.rabbitminers.buildersbundle.util.InventoryUtil;
 import dev.architectury.event.events.client.ClientTickEvent;
 import dev.architectury.event.events.common.BlockEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 
 public class ClientEvents {
     public static void init() {
@@ -18,6 +25,18 @@ public class ClientEvents {
             while (BuildersBundleKeymaps.CYCLE_BACKWARDS.consumeClick() || BuildersBundleKeymaps.CYCLE_FORWARDS.consumeClick()) {
                 SatchelItem.cycleSelectedBlock(minecraft,
                         BuildersBundleKeymaps.CYCLE_FORWARDS.isDown());
+            }
+        });
+
+        ClientTickEvent.CLIENT_POST.register(minecraft -> {
+            while (BuildersBundleKeymaps.CHANGE_PLACEMENT_MODE.consumeClick()) {
+                Player player = Minecraft.getInstance().player;
+                if (player == null) break;
+                InteractionHand usedHand = InventoryUtil
+                        .getHandOfItem(player, BuildersBundleItems.BUILDERS_BUNDLE.get());
+                if (usedHand == null) break;
+                ItemStack bundleStack = player.getItemInHand(usedHand);
+                SatchelItem.cyclePlacementMode(bundleStack);
             }
         });
 

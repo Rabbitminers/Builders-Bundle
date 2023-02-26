02c41244843a4c2704cc587a0aded5caf3531493
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-26 18:08:51 +0000

    Added None & Placement Classes to PlacementMode enum

--- common/src/main/java/com/rabbitminers/buildersbundle/satchel/PlacementMode.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/satchel/PlacementMode.java
@@ -1,23 +1,33 @@
 package com.rabbitminers.buildersbundle.satchel;
 
+import com.rabbitminers.buildersbundle.container.SatchelInventory;
+import com.rabbitminers.buildersbundle.satchel.modes.EmptyPlacementMode;
+import com.rabbitminers.buildersbundle.satchel.modes.IPlacementMode;
+import com.rabbitminers.buildersbundle.satchel.modes.RandomPlacementMode;
+import com.rabbitminers.buildersbundle.satchel.modes.SimplePlacementMode;
 import net.minecraft.network.chat.BaseComponent;
 import net.minecraft.network.chat.TranslatableComponent;
-
-import java.awt.*;
+import net.minecraft.world.item.ItemStack;
 
 public enum PlacementMode {
-    SIMPLE(new TranslatableComponent("buildersbundle.placement.simple")),
-    RANDOMIZED(new TranslatableComponent("buildersbundle.placement.random"))
+    NONE(new TranslatableComponent("buildersbundle.placement.none"), new EmptyPlacementMode()),
+    SIMPLE(new TranslatableComponent("buildersbundle.placement.simple"), new SimplePlacementMode()),
+    RANDOMIZED(new TranslatableComponent("buildersbundle.placement.random"), new RandomPlacementMode())
     ;
 
     private final BaseComponent name;
+    private final IPlacementMode mode;
 
-    PlacementMode(BaseComponent name) {
+    PlacementMode(BaseComponent name, IPlacementMode mode) {
         this.name = name;
+        this.mode = mode;
     }
 
     public PlacementMode cycleMode(PlacementMode mode) {
         return values()[(ordinal() + 1) % values().length];
     }
 
+    public ItemStack getStackForPlacement(SatchelInventory inventory) {
+        return this.mode.getBlockForPlacement(inventory);
+    }
 }

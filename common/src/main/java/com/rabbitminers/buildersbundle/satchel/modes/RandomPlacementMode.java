2b8827896dae8e6633cadc1738c1018ddc5cd9e6
Charlie Wood <rabbitminers2.0@gmail.com>
2023-02-27 16:51:24 +0000

    Fixed the rave induced by running randomisation on each frame render

--- common/src/main/java/com/rabbitminers/buildersbundle/satchel/modes/RandomPlacementMode.java
+++ common/src/main/java/com/rabbitminers/buildersbundle/satchel/modes/RandomPlacementMode.java
@@ -1,21 +1,27 @@
 package com.rabbitminers.buildersbundle.satchel.modes;
 
 import com.rabbitminers.buildersbundle.container.SatchelInventory;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.state.BlockState;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Random;
 
 public class RandomPlacementMode implements IPlacementMode {
+    public static int getCurrentSecond() {
+        LocalDateTime now = LocalDateTime.now();
+        return now.getSecond();
+    }
+
     @Override
     public @NonNull ItemStack getBlockForPlacement(SatchelInventory inventory) {
         List<ItemStack> items = inventory.getAllItems();
         if (items.isEmpty())
             return ItemStack.EMPTY;
         Random random = new Random();
+        // Use current second to prevent the rave that occurs from calling this each second
+        random.setSeed(getCurrentSecond());
         int randomIndex = random.nextInt(items.size());
         return items.get(randomIndex);
     }
